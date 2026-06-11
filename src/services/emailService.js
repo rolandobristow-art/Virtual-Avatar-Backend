8b257a7baf477cf99afe9b916183409438563049
Roland Bristow <rolando.bristow@gmail.com>
2026-06-11 10:47:48 +0200

    Change mail transporter

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,11 +1,17 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_APP_PASSWORD,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 export async function sendLeadEmail(lead) {
